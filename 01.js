0957589d1a1cd60b87667efd10f26335ac89599f
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-06-08 08:15:32 +0300

    second commit

--- 01.js
+++ 01.js
@@ -113,24 +113,93 @@
 // );
 
 //01 
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <div className="UserInfo">
-                <img className="Avatar"
-                    src={props.author.avatarUrl}
-                    src={props.author.name}
-                />
-                <div className="UserInfo-name">
-                    {props.author.name}
-                </div>
+// function Comment(props) {
+//     return (
+//         <div className="Comment">
+//             <div className="UserInfo">
+//                 <img className="Avatar"
+//                     src={props.author.avatarUrl}
+//                     src={props.author.name}
+//                 />
+//                 <div className="UserInfo-name">
+//                     {props.author.name}
+//                 </div>
+//             </div>
+//             <div className="Comment-text">
+//                 {props.text}
+//             </div>
+//             <div className="Comment-date">
+//                 {formatDate(props.date)}
+//             </div>
+//         </div>
+//     );
+// }
+
+// STATE AND LIFECYCLE
+// 01
+// function Clock(props) {
+//     return (
+//         <div>
+//             <h1>Hello, world!</h1>
+//             <h2>It is {props.date.toLocaleString()}</h2>
+//         </div>
+//     );
+// }
+
+// function tick() {
+//     ReactDOM.render(
+//         <Clock date={new Date()} />
+//         document.getElementById('root')
+//     );
+// }
+
+// setInterval(tick, 1000);
+
+//01
+// class Clock extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello, world!</h1>
+//                 <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+//             </div>
+//         );
+//     }
+// }
+
+//03
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    
+    componentWillMount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
